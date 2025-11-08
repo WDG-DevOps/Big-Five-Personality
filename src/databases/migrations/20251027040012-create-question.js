b7d7998444e3c27f1b3ea69a747be58387a7dc9b
Johanes Nicky C. S <apanyaclay1@gmail.com>
2025-11-08 13:34:56 +0700

    create

---
+++ src/databases/migrations/20251027040012-create-question.js
@@ -0,0 +1,38 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      question_text: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      trait: {
+        type: Sequelize.ENUM('O', 'C', 'E', 'A', 'N'),
+        allowNull: false
+      },
+      is_reverse_scored: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('questions');
+  }
+};
